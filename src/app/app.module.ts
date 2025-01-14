97f78b1426f9511809ccbd0a095b0762bea50a0d
CharlieCRChen <117780398+CRChenND@users.noreply.github.com>
2025-01-14 20:02:38 +0900

    change icon image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './index/home.component';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 import { AppRoutingModule } from './app.routing';
 import { FittsTestComponent } from './fitts-test/fitts-test.component';
